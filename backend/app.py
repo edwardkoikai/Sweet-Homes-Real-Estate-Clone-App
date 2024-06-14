1a06ecf9e16c46b684110f5599219ec4db4f6066
edwardkoikai <edwardkoikai@gmail.com>
2024-06-15 01:42:49 +0300

    setup backend

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class PropertyModel(BaseModel):
+    name: str 
+    description: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/property_list")
+def get_property_list():
+    return [{"name": "Runda"}]
+
+@app.post("/property_list")
+def save_property_list(data: PropertyModel):
+    print(data)
