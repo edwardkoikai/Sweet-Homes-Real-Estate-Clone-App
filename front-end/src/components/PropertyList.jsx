c9c440db3c9c1bb725b5b85b8f5871b20e67f79e
edwardkoikai <edwardkoikai@gmail.com>
2024-06-15 14:45:06 +0300

    fix grid layout

--- front-end/src/components/PropertyList.jsx
+++ front-end/src/components/PropertyList.jsx
@@ -10,7 +10,7 @@ function PropertyList() {
     <Container>
       <Row className="mt-10" style={{ marginTop: 10 }}>
       {new Array(12).fill(Math.random()).map((_, index) => (
-        <Col key={index} className="mb-5" xs={6} md={4}>
+        <Col key={index} className="mb-5">
           <PropertyCard />
         </Col>
       ))}
